0aa37fdfb9cea3dc02b3d878ac664bba5301ca43
Steve Bradley <stevebradley@gmail.com>
2019-01-12 10:38:32 -0500

    use local volume driver and put container names in ENV

--- app/views/bitcoin.py
+++ app/views/bitcoin.py
@@ -7,7 +7,11 @@ bp = Blueprint('bitcoin', __name__, url_prefix='/bitcoin')
 @bp.route('/')
 def index():
     rpc_connection = AuthServiceProxy(
-        "http://%s:%s@bitcoin:8332"%(os.environ['RPC_USER'], os.environ['RPC_PASSWORD'])
+        "http://%s:%s@%s:8332"%(
+            os.environ['RPC_USER'],
+            os.environ['RPC_PASSWORD'],
+            os.environ['BITCOIN_CONTAINER']
+        )
     )
     best_block_hash = rpc_connection.getbestblockhash()
     info = rpc_connection.getblock(best_block_hash)
