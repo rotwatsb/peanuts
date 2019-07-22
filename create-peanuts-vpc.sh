92d9eb8b8a791eac6a7a9f1d6fb896d7c5f7c5be
Steve Bradley <steve@debian8svm.debian8svm>
2019-07-21 21:59:12 -0400

    create cloudformation templates

---
+++ create-peanuts-vpc.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+aws cloudformation create-stack --stack-name=peanuts-vpc-stack --template-body file://cloudformation/peanuts-vpc.json --parameters ParameterKey=AllowedSSHCidr,ParameterValue=$(dig +short $1)
