3a8138434c2e1891119a98c7700a12f52831343d
Steve Bradley <stevebradley@gmail.com>
2019-08-28 00:48:27 -0400

    use requests lib instead of bitcoi-jsonrpc

--- peanuts/views/bitcoin.py
+++ peanuts/views/bitcoin.py
@@ -1,18 +1,19 @@
-import os
+from peanuts.lib import btc
 from flask import Blueprint, render_template, jsonify
-from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
+
 
 bp = Blueprint('bitcoin', __name__, url_prefix='/bitcoin')
 
 @bp.route('/')
 def index():
-    rpc_connection = AuthServiceProxy(
-        "http://%s:%s@%s:8332"%(
-            os.environ['RPC_USER'],
-            os.environ['RPC_PASSWORD'],
-            os.environ['RPC_BITCOIN_HOST']
-        )
-    )
-    best_block_hash = rpc_connection.getbestblockhash()
-    info = rpc_connection.getblock(best_block_hash)
+    best_block_hash = btc.rpc(
+        method='getblockchaininfo',
+        params=[]
+    )['result']['bestblockhash'];
+
+    info = btc.rpc(
+        method='getblock',
+        params=[best_block_hash]
+    )['result']
+
     return render_template('bitcoin/index.html', info=info)
