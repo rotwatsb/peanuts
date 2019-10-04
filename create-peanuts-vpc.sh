91e37eeb6f1e722f1cacba4d66468c5e7e5a5a6c
Steve Bradley <steve@debian8svm.debian8svm>
2019-10-04 10:29:49 -0400

    factor out bitcoind, use service discovery

--- create-peanuts-vpc.sh
+++ create-peanuts-vpc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws cloudformation create-stack --stack-name=peanuts-vpc-stack --template-body file://cloudformation/peanuts-vpc.json --parameters ParameterKey=AllowedSSHCidr,ParameterValue=$(dig +short $1)/32
+aws cloudformation create-stack --stack-name=peanuts-vpc-stack --template-body file://cloudformation/peanuts-vpc.json
--- run_peanuts_local_docker.sh
+++ run_peanuts_local_docker.sh
@@ -5,6 +5,5 @@ docker run \
        -e RPC_USER='localdev' \
        -e RPC_PASSWORD='localdev' \
        -e RPC_BITCOIN_HOST='localhost' \
-       -e APACHE_SERVER_NAME='localhost' \
        -p 5000:5000 \
        peanuts
