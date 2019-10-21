1adf261b3ee7d31c758cb81bc59e16fdad74cc90
Steve Bradley <steve@debian8svm.debian8svm>
2019-10-20 20:41:42 -0400

    show outputs from cloudformation scripts

--- create-peanuts-vpc.sh
+++ create-peanuts-vpc.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 aws cloudformation create-stack --stack-name=peanuts-vpc-stack --template-body file://cloudformation/peanuts-vpc.json
+
+STACK_STATUS="\"CREATE_IN_PROGRESS\""
+while [[ "$STACK_STATUS" == "\"CREATE_IN_PROGRESS\"" ]]
+do
+    echo "Stack status: $STACK_STATUS"
+    sleep 10
+    STACK_STATUS=$(aws cloudformation describe-stacks --stack-name=peanuts-vpc-stack | jq '.["Stacks"][0]["StackStatus"]')
+done
+
+if [ "$STACK_STATUS" == "\"CREATE_COMPLETE\"" ]; then
+   aws cloudformation describe-stacks --stack-name=peanuts-vpc-stack | jq '.["Stacks"][0]["Outputs"]'
+fi
